6caa1ce0c8a516487c602a55743bfaece45faa94
Benjamin Mehanovic <33999334+benjom22@users.noreply.github.com>
2023-05-09 11:57:17 +0200

    ini2

---
+++ verify.php
@@ -0,0 +1,20 @@
+<?php
+$data = array(
+            'secret' => "0xcc5268aC068Bcf0E89ADF177AAbD0F030d05892",
+            'response' => $_POST['h-captcha-response']
+        );
+$verify = curl_init();
+curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+curl_setopt($verify, CURLOPT_POST, true);
+curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($verify);
+var_dump($response);
+$responseData = json_decode($response);
+if($responseData->success) {
+    // your success code goes here
+} 
+else {
+   // return error to user; they did not pass
+}
+?>
